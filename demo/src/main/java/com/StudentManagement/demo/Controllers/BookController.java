a9871cbb79bebd6099a5fc7b4d10a20bb8441cc3
VarunPareek <114807315+varunpareek690@users.noreply.github.com>
2024-05-31 00:43:03 +0530

    Put API to get student from Book ID

--- demo/src/main/java/com/StudentManagement/demo/Controllers/BookController.java
+++ demo/src/main/java/com/StudentManagement/demo/Controllers/BookController.java
@@ -1,17 +1,29 @@
 package com.StudentManagement.demo.Controllers;
 
 import com.StudentManagement.demo.BookDetails;
+import com.StudentManagement.demo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.awt.print.Book;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 public class BookController {
 //    List<BookDetails> bookDetailsList = new ArrayList<>();
+
+    @PutMapping("/books/{ID}")
+    public String updateBookDetailswithStudentId(@PathVariable int ID, @RequestBody Student student){
+        Optional<BookDetails> bookDetailsOptional = bookRepository.findById(ID);
+        if(bookDetailsOptional.isPresent()){
+            BookDetails book = bookDetailsOptional.get();
+            book.setStudent(student);
+            bookRepository.save(book);
+            return "Student Id Updated Successfully";
+        }
+        else return "Error! Something is Wrong";
+    }
 
 
     @Autowired
@@ -40,6 +52,19 @@ public class BookController {
         } else {
             return "Book not found";
         }
+    }
+
+
+    @GetMapping("/books/student")
+    public Map<String,Student> getStudentfromBook(@RequestParam int ID){
+        Optional<BookDetails> bookDetailsOptional = bookRepository.findById(ID);
+        if(bookDetailsOptional.isPresent()){
+            Map<String,Student> map = new HashMap<>();
+            BookDetails book = bookDetailsOptional.get();
+            map.put(book.getBookName(),book.getStudent());
+            return map;
+        }
+        return null;
     }
 
 }
