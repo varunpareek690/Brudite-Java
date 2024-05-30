3dab6b502e951aa069231c9406c37517cb6be269
VarunPareek <114807315+varunpareek690@users.noreply.github.com>
2024-05-31 00:45:52 +0530

    Updated

--- demo/src/main/java/com/StudentManagement/demo/Controllers/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.StudentManagement.demo.Controllers;
-
-import com.StudentManagement.demo.BookDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookDetails,Integer> {
-}
--- demo/src/main/java/com/StudentManagement/demo/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.StudentManagement.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-}
